68def65f833d2bc08a7cd61396e66aa2fd0c89f7
lining <940409607@qq.com>
2024-01-14 16:09:30 +0800

    创建Springboot项目

---
+++ src/main/java/com/example/programtest/ProgramtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.programtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/programtest/ProgramtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
